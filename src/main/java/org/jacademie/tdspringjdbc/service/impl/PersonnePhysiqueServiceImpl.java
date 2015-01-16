6252e31371de8984f9cd8b34e857ce7cb9ad47cd
rgirodon <rgirodon@sqli.com>
2015-01-16 15:50:22 +0100

    Implementation du service

---
+++ src/main/java/org/jacademie/tdspringjdbc/service/impl/PersonnePhysiqueServiceImpl.java
@@ -0,0 +1,34 @@
+package org.jacademie.tdspringjdbc.service.impl;
+
+import java.util.Collection;
+
+import org.jacademie.tdspring.dao.PersonnePhysiqueDao;
+import org.jacademie.tdspring.domain.PersonnePhysique;
+import org.jacademie.tdspring.service.PersonnePhysiqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("personnePhysiqueService")
+public class PersonnePhysiqueServiceImpl implements PersonnePhysiqueService {
+
+	@Autowired
+	private PersonnePhysiqueDao personnePhysiqueDao;
+	
+	@Override
+	public PersonnePhysique findPersonnePhysiqueByNrc(String nrc) {
+		
+		return this.personnePhysiqueDao.findPersonnePhysiqueByNrc(nrc);
+	}
+
+	@Override
+	public void createPersonnePhysique(PersonnePhysique personnePhysique) {
+		
+		this.personnePhysiqueDao.createPersonnePhysique(personnePhysique);
+	}
+
+	@Override
+	public Collection<PersonnePhysique> findAllPersonnePhysiques() {
+		
+		return this.personnePhysiqueDao.findAllPersonnePhysiques();
+	}
+}
